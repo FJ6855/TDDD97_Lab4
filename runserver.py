7ee78ab17a65101e1a6fcf811a789f263d87a5b3
Sebatian Wikström <lars.sebastian.wikstrom@gmail.com>
2016-03-14 14:36:46 +0100

    Removed commented code not being used in runserver.py.

--- runserver.py
+++ runserver.py
@@ -7,12 +7,3 @@ from Twidder import app
 app.debug = True
 http_server = WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
 http_server.serve_forever()
-
-#ef websocket_app(environ, start_response):
-#    if environ["PATH_INFO"] == '/echo':
-#        ws = environ["wsgi.websocket"]
-#        message = ws.receive()
-#        ws.send(message)
-
-#server = pywsgi.WSGIServer(("", 8000), websocket_app, handler_class=WebSocketHandler)
-#server.serve_forever()
